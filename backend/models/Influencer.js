3f9b8cc1d53ea5c68e86560c409db23bdc6e1f59
anupamxy <123785384+anupamxy@users.noreply.github.com>
2023-09-09 22:51:49 +0530

    server

---
+++ backend/models/Influencer.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const {Schema} = mongoose;
+ 
+//schema
+const NotesSchema = new Schema({
+    user:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user'
+    },
+
+    username:{
+        type: String,
+        required: true
+    },
+    name:{
+        type: String,
+        required: true
+    },
+    inemail:{
+        type :  String,
+        required: true,
+    },
+    
+    phone:{
+        type: Number,
+        required: true,
+    },
+    date:{
+        type: Date,
+        default : Date.now
+    }
+});
+
+module.exports = mongoose.model('notes',NotesSchema);
